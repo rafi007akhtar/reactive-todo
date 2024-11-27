446223bf49fc988b24442d8002154bd912f1dd61
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-11-28 00:10:25 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.scss";
+import NewTask from "./components/NewTask";
+
+function App() {
+  return (
+    <div className="app grid gap-4">
+      <aside className="side-bar col-start-1 col-end-3">
+        This will hold the categories
+      </aside>
+
+      <NewTask />
+
+      <main className="main-content mt-8">
+        This will hold the task list <br />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewTask.tsx
@@ -0,0 +1,5 @@
+function NewTask() {
+  return <div className="new-task row-start-3 row-end-4">New task</div>;
+}
+
+export default NewTask;
